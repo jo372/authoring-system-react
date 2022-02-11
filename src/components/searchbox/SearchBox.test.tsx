3e5adc35084b647f175eee70fd45941b0f9ada9e
Jordan O'Hara <J.O'Hara2@uni.brighton.ac.uk>
2022-02-11 14:25:06 +0000

    initial commit of the search box testing class which tests the onSearch props and placeholder.

--- src/components/searchbox/SearchBox.test.tsx
+++ src/components/searchbox/SearchBox.test.tsx
@@ -2,36 +2,37 @@ import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import SearchBox from './SearchBox';
 
-test('when searchBox is provided with an onSearch function, it should be called when button is pressed', () => {
-  const onSearch = jest.fn();
-  const { container } = render(<SearchBox onSearch={onSearch} />);
-  const input = container.querySelector('input') as HTMLInputElement;
-  const button = container.querySelector('button') as HTMLButtonElement;
+describe('SearchBox', () => {
+  it('when searchBox is provided with an onSearch function, it should be called when button is pressed', () => {
+    const onSearch = jest.fn();
+    const { container } = render(<SearchBox onSearch={onSearch} />);
+    const input = container.querySelector('input') as HTMLInputElement;
+    const button = container.querySelector('button') as HTMLButtonElement;
 
-  fireEvent.change(input, { target: { value: 'Hello' } });
-  fireEvent.click(button);
+    fireEvent.change(input, { target: { value: 'Hello' } });
+    fireEvent.click(button);
 
-  expect(onSearch).toHaveBeenCalledWith('Hello');
+    expect(onSearch).toHaveBeenCalledWith('Hello');
+  });
+
+  it('when a SearchBox function is not provided it should not call the function', () => {
+    const onSearch = jest.fn();
+    const { container } = render(<SearchBox />);
+    const input = container.querySelector('input') as HTMLInputElement;
+    const button = container.querySelector('button') as HTMLButtonElement;
+
+    fireEvent.change(input, { target: { value: 'Hello' } });
+    fireEvent.click(button);
+    expect(onSearch).not.toHaveBeenCalled();
+  });
+
+  test('after enter has been pressed the searchbox value should be set to blank', () => {
+    const onSearch = jest.fn();
+    const { container } = render(<SearchBox onSearch={onSearch} />);
+    const input = container.querySelector('input') as HTMLInputElement;
+
+    fireEvent.change(input, { target: { value: 'Hello' } });
+    fireEvent.keyDown(input, { key: 'Enter', code: 13 });
+    expect(input.value).toBe('');
+  });
 });
-
-test('when a SearchBox function is not provided it should not call the function', () => {
-  const onSearch = jest.fn();
-  const { container } = render(<SearchBox />);
-  const input = container.querySelector('input') as HTMLInputElement;
-  const button = container.querySelector('button') as HTMLButtonElement;
-
-  fireEvent.change(input, { target: { value: 'Hello' } });
-  fireEvent.click(button);
-  expect(onSearch).not.toHaveBeenCalled();
-});
-
-
-test('after enter has been pressed the searchbox value should be set to blank', () => {
-  const onSearch = jest.fn();
-  const { container } = render(<SearchBox onSearch={onSearch} />);
-  const input = container.querySelector('input') as HTMLInputElement;
-
-  fireEvent.change(input, { target: { value: 'Hello' } });
-  fireEvent.keyDown(input, { key: 'Enter', code: 13 });
-  expect(input.value).toBe('');
-})
