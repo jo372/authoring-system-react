8f49c4994750c65add13ca63273c43d1a8b6676e
Jordan O'Hara <J.O'Hara2@uni.brighton.ac.uk>
2022-02-11 14:21:14 +0000

    updated to use describe and it instead of just test.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,19 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('the document should contain a CollapisbleMenu', () => {
-  const { container } = render(<App />);
-  expect(container.querySelector('.collapsible-menu')).not.toBeNull();
-});
+describe('App Test', () => {
+  it('the document should contain a CollapisbleMenu', () => {
+    const { container } = render(<App />);
+    expect(container.querySelector('.collapsible-menu')).not.toBeNull();
+  });
+  
+  it('within the collapsible container, there should be a search box', () => {
+    const { container } = render(<App />);
+    const collapsible_menu = container.querySelector('.collapsible-menu') as HTMLDivElement;
+    expect(collapsible_menu.querySelector('.searchbox')).not.toBeNull();
+  })
+  
+  it('when enter is pressed in the searchbox, it should console log the value.', () => {
+  
+  })
+})
