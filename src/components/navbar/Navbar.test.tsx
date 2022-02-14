6c17c997d318b42511c179feca195b701f09b461
Jordan O'Hara <J.O'Hara2@uni.brighton.ac.uk>
2022-02-14 11:22:31 +0000

    basic navbar test class which tests for properties id, className and children.

---
+++ src/components/navbar/Navbar.test.tsx
@@ -0,0 +1,22 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import Navbar from './Navbar';
+
+describe('Navbar', () => {
+  it('should render', () => {
+    const { container } = render(<Navbar />);
+    expect(container).toBeTruthy();
+  });
+  it('when a child is added to the component, it should be added.', () => {
+    const { container } = render(<Navbar><div id="hello">Hello</div></Navbar>);
+    expect(container.querySelector('#hello')).toBeTruthy();
+  });
+  it('when an id is passed in the navbar should contain the property', () => {
+    const { container } = render(<Navbar id="hello" />);
+    expect(container.querySelector('#hello')).toBeTruthy();
+  })
+  it('when a className is provided, expect the navbar to contain the css classes', () => {
+    const { container } = render(<Navbar className="hello" />);
+    expect(container.querySelector('.hello')).toBeTruthy();
+  })
+});
