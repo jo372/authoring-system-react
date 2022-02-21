9a422d5f658a7f32590a5b67f5b063467a1b0774
Jordan O'Hara <J.O'Hara2@uni.brighton.ac.uk>
2022-02-21 09:22:08 +0000

    remove redundant comments

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -16,15 +16,10 @@ describe('App Test', () => {
   it('if there is device__preview-button with .active, it should remove it when another button is pressed', () => {
     const { container } = render(<App />);
     const currentPreviewDevice = container.querySelector('.device__preview-button.active') as HTMLDivElement;
-    const anotherPreviewDeviceButton = container.querySelector('.device__preview-button:not(.active)') as HTMLDivElement;  
-     
+    const anotherPreviewDeviceButton = container.querySelector('.device__preview-button:not(.active)') as HTMLDivElement;
+
     expect(currentPreviewDevice.classList.contains('active')).toBe(true);
     fireEvent.click(anotherPreviewDeviceButton);
     expect(currentPreviewDevice.classList.contains('active')).toBe(false);
-    // const { container } = render(<App />);
-    // const previewDeviceButton = container.querySelector('.device__preview-button[data-type="desktop"]') as HTMLButtonElement;
-    // expect(previewDeviceButton.classList.contains('active')).toBe(true);
-    // fireEvent.click(previewDeviceButton);
-    // expect(previewDeviceButton.classList.contains('active')).toBe(false);
-  })
+  });
 });
