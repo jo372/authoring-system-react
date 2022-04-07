773d229c2ff5816523fdb6d493a1d8dbb4c4f6d8
Jordan O'Hara <J.O'Hara2@uni.brighton.ac.uk>
2022-04-07 22:27:54 +0100

    a typescript file which holds all the creatable element types.

---
+++ src/util/CreatableElementType.ts
@@ -0,0 +1,21 @@
+export default class CreatableElementType {
+  public static readonly TEXT = 'text';
+
+  public static readonly CONTAINER = 'container';
+
+  public static readonly IMAGE = 'image';
+
+  public static readonly GALLERY = 'gallery';
+
+  public static readonly LIST = 'list';
+
+  public static readonly LINK = 'link';
+
+  public static readonly YOUTUBE_VIDEO = 'youtube_video';
+
+  public static readonly FOOTER = 'footer';
+
+  public static readonly NAVBAR = 'navbar';
+
+  public static readonly BUTTON = 'button';
+}
